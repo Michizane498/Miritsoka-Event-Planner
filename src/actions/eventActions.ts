ec977f2a7e425cf781d63ac6aa48ce7d3043e694
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-03-31 00:33:48 +0300

    Update Func

--- src/actions/eventActions.ts
+++ src/actions/eventActions.ts
@@ -6,7 +6,7 @@ import { events, InsertEvent } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 
 export const getEvents = async () => {
-  return await db.select().from(events);
+  return await db.select().from(events).orderBy(events.date);
 };
 
 export const addEvents = async (data: InsertEvent) => {
@@ -35,9 +35,22 @@ export const deleteSpecificEvents = async (event: number) => {
   await db.delete(events).where(eq(events.id, event));
   revalidatePath("/");
 };
-export const updateEvents = async (data: InsertEvent) => {
+
+
+export const updateEvents = async (data: {
+  id: number;
+  client: string;
+  place: string;
+  date: string;
+  travel?: number | null;
+  materials: string;
+  observation?: string | null;
+  focal: string;
+  confirmation: string;
+}) => {
   revalidatePath("/");
-  data.id? await db
+  
+  await db
     .update(events)
     .set({
       client: data.client,
@@ -49,7 +62,5 @@ export const updateEvents = async (data: InsertEvent) => {
       focal: data.focal,
       confirmation: data.confirmation,
     })
-    .where(eq(events.id, data.id))
-    : null
-
+    .where(eq(events.id, data.id));
 };
