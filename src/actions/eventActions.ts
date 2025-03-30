c9fae14d60ce94f21f7a7fb8a682b46120b17ccc
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-03-30 10:58:03 +0300

    DateClick

--- src/actions/eventActions.ts
+++ src/actions/eventActions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/db/drizzle";
+import { eq } from "drizzle-orm";
 import { events, InsertEvent } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 
@@ -8,6 +9,7 @@ export const getEvents = async () => {
   const data = await db.select().from(events);
   return data;
 };
+
 export const addEvents = async (data: InsertEvent) => {
   await db.insert(events).values({
     client: data.client,
@@ -20,4 +22,12 @@ export const addEvents = async (data: InsertEvent) => {
     confirmation: data.confirmation,
   });
   revalidatePath("/");
+};
+
+export const getSpecificEvents = async (date: Date) => {
+  const stringdate = date.toISOString().split('T')[0]
+  return await db
+    .select()
+    .from(events)
+    .where(eq(events.date, stringdate));
 };
--- src/components/Calendar/ColoredCalendar.tsx
+++ src/components/Calendar/ColoredCalendar.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import { fr } from "date-fns/locale";
-import {
-  ArrowDown,
-  ArrowLeft,
-  ArrowRight,
-  ChevronDown,
-  ChevronLeft,
-  ChevronRight,
-  Circle,
-} from "lucide-react";
+import { ChevronDown, ChevronLeft, ChevronRight, Circle } from "lucide-react";
 import * as React from "react";
 
 import { DayPicker, getDefaultClassNames } from "react-day-picker";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { isSameDay } from "date-fns";
+import { getSpecificEvents } from "@/actions/eventActions";
 
 export function CustomCalendar() {
-  const [selected, setSelected] = React.useState<Date | undefined>(undefined);
+  const [selected, setSelected] = React.useState<Date>(new Date());
+
+  const [allbrownCells, setBrownCells] = useState<Date[]>([]);
+  const [allgreenCells, setGreenCells] = useState<Date[]>([]);
+  const [allredCells, setRedCells] = useState<Date[]>([]);
+  const [data, setData] = useState<Date[]>([]);
 
   const today = new Date();
 
@@ -33,34 +32,39 @@ export function CustomCalendar() {
     color: string;
   }
 
-  const handledayclick = () => {
-    console.log(selected)
-  }
-  
-  const [allbrownCells, setBrownCells] = useState<Date[]>([]);
-  const [allgreenCells, setGreenCells] = useState<Date[]>([]);
-  const [allredCells, setRedCells] = useState<Date[]>([]);
+  const handledayclick = async (date: Date) => {
+    setSelected(date);
+    const found = data.find((d) => isSameDay(d, date!));
+    if (found) {
+      const details = await getSpecificEvents(found);
+      // console.log("Selected date:", date, "Found:", found, "le izy", leizy);
+    } else {
+      return;
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("/api/calendar");
         const data = (await response.json()) as CalendarEvent[];
+        const select = data.map((item: any) => new Date(item.date));
+        setData(select);
 
         const brown = data
           .filter((item: any) => item.color === "Miritsoka")
           .map((item: any) => new Date(item.date));
-        setBrownCells(Object.assign(brown));
+        setBrownCells(brown);
 
         const green = data
           .filter((item: any) => item.color === "Confirmé")
           .map((item: any) => new Date(item.date));
-        setGreenCells(Object.assign(green));
+        setGreenCells(green);
 
         const red = data
           .filter((item: any) => item.color === "Non confirmé")
           .map((item: any) => new Date(item.date));
-        setRedCells(Object.assign(red));
+        setRedCells(red);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -72,12 +76,12 @@ export function CustomCalendar() {
   return (
     <div className="flex flex-col">
       <DayPicker
-      
+        required={true}
         mode="single"
         locale={fr}
-        onSelect={setSelected}
-        selected={selected}
         onDayClick={handledayclick}
+        // onSelect={setSelected}
+        selected={selected}
         modifiers={{
           brownCells: allbrownCells,
           greenCells: allgreenCells,
@@ -104,9 +108,9 @@ export function CustomCalendar() {
             const { day, ...buttonProps } = props;
             return (
               <Button
-                variant={"ghost"}
                 {...buttonProps}
-                className="lg:w-15 w-10 h-10 lg:h-15 px-3 py-3 sm:gap-0 text-accent-foreground group-aria-selected:bg-orange-700 rounded-sm"
+                variant={"ghost"}
+                className="lg:w-15 w-10 h-10 lg:h-15 px-3 py-3 sm:gap-0 text-accent-foreground group-aria-selected:bg-accent rounded-sm"
               />
             );
           },
